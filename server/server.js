64c5d1c1e23f3c5bfbdc9e5201acb69106c3d2a0
bartek <boratek.3@gmail.com>
2018-01-21 20:38:05 +0100

    ch-106 - add index route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const path = require('path');
+const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 
-console.log(__dirname + '/../public');
-console.log(publicPath);
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
