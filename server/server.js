1a6404f2f71799eb48654adb71424ca462e8af4b
bartek <boratek.3@gmail.com>
2018-01-23 18:45:21 +0100

    ch-107 - use io in server

--- server/server.js
+++ server/server.js
@@ -8,8 +8,11 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on();
 
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
