11cd549feee7ffdd0f995c0946a967ae2fe5f8cc
bartek <boratek.3@gmail.com>
2018-01-24 13:13:50 +0100

    ch-108 - create newEmail event

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,14 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
+
+  socket.emit('createEmail')
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function (email) {
+  console.log('New email', email);
 });
--- server/server.js
+++ server/server.js
@@ -15,6 +15,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newEmail', {
+    from: 'bart@server.js',
+    text: 'Hello Alice',
+    createdAt: 123
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
