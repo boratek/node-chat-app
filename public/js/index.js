c474b79712ee134b103711473482dd489dca7068
bartek <boratek.3@gmail.com>
2018-01-29 23:10:02 +0100

    ch-113 - add messages to list

--- public/js/index.js
+++ public/js/index.js
@@ -15,13 +15,10 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('New Message:', message);
-});
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
 
-socket.emit('createMessage', {
-  from: 'Bart',
-  text: 'Hello'
-}, function (msg) {
-  console.log('Works!', msg);
+  jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
