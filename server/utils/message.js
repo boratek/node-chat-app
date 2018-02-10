afdea92f5185492b95bf9cfad1a44d417330cfa9
bartek <boratek.3@gmail.com>
2018-02-10 12:27:51 +0100

    ch-118 - show time in chat window

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,12 +1,23 @@
+var moment = require('moment');
+
 var generateMessage = (from, text) => {
-  return {from, text, createdAt: new Date().getTime()};
+  var timestamp = moment().valueOf();
+  var date = moment(timestamp);
+  var createdAt = date.format('h:mm a');
+
+  return {from, text, createdAt: timestamp};
 };
 
 var generateLocationMessage = (from, latitude, longitude) => {
+  var timestamp = moment().valueOf();
+  var date = moment(timestamp);
+  var createdAt = date.format('h:mm a');
+
   return {
     from,
     url: `https://www.google.com/maps?q${latitude},${longitude}`,
-    createdAt: new Date().getTime()}
+    createdAt: timestamp
+  }
 };
 
 module.exports = {generateMessage, generateLocationMessage};
